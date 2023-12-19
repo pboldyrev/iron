702e83c7201b658729630f7b215cad406d9fc43a
Paul <impaulboldyrev@gmail.com>
2023-12-19 09:14:17 -0800

    Added chart to asset details

--- iron/src/app/pages/asset-details-page/value-history-chart/value-history-chart.component.ts
+++ iron/src/app/pages/asset-details-page/value-history-chart/value-history-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-value-history-chart',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './value-history-chart.component.scss'
 })
 export class ValueHistoryChartComponent {
+  public chart: Chart<any> | undefined;
 
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  private createChart(): void {
+    this.chart = new Chart("valueHistoryChart", {
+      type: 'line',
+
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13', '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92', '574', '573', '576'],
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      }
+    });
+  }
 }
