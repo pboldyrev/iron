90927fbe4b3b4b5ccaab90a8cb1ad6e7a0321234
Paul <impaulboldyrev@gmail.com>
2023-12-18 18:51:38 -0800

    Added basic chart

--- iron/src/app/chart/chart.component.ts
+++ iron/src/app/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
@@ -9,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './chart.component.scss'
 })
 export class ChartComponent {
+  public chart: Chart<any> | undefined;
 
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  private createChart(): void {
+  
+    this.chart = new Chart("chart", {
+      type: 'line',
+
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13', '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92', '574', '573', '576'],
+          }
+        ]
+      },
+
+      options: {
+        responsive: true,
+      }
+    });
+  }
 }
