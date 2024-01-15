eece04fb018f92e80d2a4e8f656cd5fd185b453f
Paul <impaulboldyrev@gmail.com>
2024-01-14 19:04:11 -0800

    Added better stock tooltips

--- iron/src/app/shared/components/forms/add-stock-form/add-stock-form.strings.ts
+++ iron/src/app/shared/components/forms/add-stock-form/add-stock-form.strings.ts
@@ -1,6 +1,8 @@
 export const TEXTS = {
     TICKER_LABEL: "Ticker",
-    DATE_LABEL: "Lot purchase date",
-    TICKER_TOOLTIP: "The ticker will be used to fetch the latest stock price information from NYSE and NASDAQ.",
-    DATE_TOOLTIP: "The date when you initially purchased the stock.",
+    DATE_LABEL: "Initial purchase date",
+    UNITS_LABEL: "Initial shares",
+    TICKER_TOOLTIP: "The ticker will be used to fetch the latest price information from NYSE and NASDAQ.",
+    DATE_TOOLTIP: "The date when you first purchased shares of this stock/ETF.",
+    UNITS_TOOLTIP: "The number of shares you initially purchased. You will be able to add changes in units over time after the asset is created.",
 }
