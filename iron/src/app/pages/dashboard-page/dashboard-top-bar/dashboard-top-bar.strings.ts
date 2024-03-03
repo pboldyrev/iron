ee5edebe89e63129ee3093da42b9a546f47beb68
Paul <impaulboldyrev@gmail.com>
2024-03-02 19:10:15 -0800

    Edited button row

--- iron/src/app/pages/dashboard-page/dashboard-top-bar/dashboard-top-bar.strings.ts
+++ iron/src/app/pages/dashboard-page/dashboard-top-bar/dashboard-top-bar.strings.ts
@@ -2,6 +2,7 @@ export const TEXTS = {
   BTN_PRIMARY_CTA: 'Add',
   BTN_SECONDARY_CTA: 'Upgrade plan',
   IMPORT_CTA: 'Import items',
+  MANUAL_CTA: 'Manually add item',
   SETTINGS_CTA: 'Settings',
   LOG_OUT_CTA: 'Log out',
   FEEDBACK_CTA: 'Provide beta feedback',
